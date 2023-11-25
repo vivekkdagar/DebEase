fd1f9b9e49530a4dbc04b114a6fe61b1f8d4e385
Vivek Dagar <vivekdagar2023@gmail.com>
2023-11-25 20:36:40 +0530

    Delete web directory

--- web/Homepage/urls.py
+++
@@ -1,8 +0,0 @@
-# Homepage/urls.py
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('', views.run_page, name="run_page"),
-]
--- web/web/urls.py
+++
@@ -1,6 +0,0 @@
-# web/urls.py
-from django.urls import path, include
-
-urlpatterns = [
-    path('', include('Homepage.urls')),
-]
